ecbc08ec3c5238ae9907b145b9ee441fca11dac1
Zeineb <zeineb.chabchoub@insat.u-carthage.tn>
2020-11-05 21:20:56 +0100

    fix fact  -1

--- fact.go
+++ fact.go
@@ -4,25 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-
-	"github.com/shirou/gopsutil/mem"
-)
+	/*	"github.com/shirou/gopsutil/mem"*/)
 
 func fact(x int) int {
 	if x == 0 {
 		return 1
+	}
+	if x == -1 {
+		return -1
 	}
 	return x * fact(x-1)
 }
 
 func main() {
 	// First END POINT
-	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+	/*http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		m, _ := mem.VirtualMemory()
 		free := fmt.Sprintf("%v", m.Available)
 		w.Write([]byte(free))
 	})
-
+	*/
 	//	Second END POINT
 	http.HandleFunc("/fact", func(w http.ResponseWriter, r *http.Request) {
 		s := r.URL.Query().Get("n")
--- fact_test.go
+++ fact_test.go
@@ -5,12 +5,12 @@ import "testing"
 func TestFact(t *testing.T) {
 
 	r := fact(-1)
-	if r != 1 {
-		t.Error("Fact(0) should be -1 ")
+	if r != -1 {
+		t.Error("Fact(-1) should be -1 ")
 	}
 
 	r = fact(0)
-	if r != 1 { 	 	
+	if r != 1 {
 		t.Error("Fact(0) should be 1 ")
 	}
 
