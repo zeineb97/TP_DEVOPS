6241d08d04108f771ecbfda7410ae55f277d9de2
Zeineb <zeineb.chabchoub@insat.u-carthage.tn>
2020-11-05 21:23:56 +0100

    fix fact negative numbers

--- fact.go
+++ fact.go
@@ -7,12 +7,14 @@ import (
 	/*	"github.com/shirou/gopsutil/mem"*/)
 
 func fact(x int) int {
+	if x < -1 {
+		return -1
+	}
+
 	if x == 0 {
 		return 1
 	}
-	if x == -1 {
-		return -1
-	}
+
 	return x * fact(x-1)
 }
 
