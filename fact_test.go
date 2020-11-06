9e327eaad12a10218507d8c0ab607cf6f4e7b2ad
Zeineb <zeineb.chabchoub@insat.u-carthage.tn>
2020-11-06 16:37:52 +0100

    benchmark

--- fact_test.go
+++ fact_test.go
@@ -2,27 +2,64 @@ package main
 
 import "testing"
 
-func TestFact(t *testing.T) {
-
-	r := fact(-1)
+func TestFactRecursive(t *testing.T) {
+	r := factRecursive(-1)
 	if r != -1 {
-		t.Error("Fact(-1) should be -1 ")
+		t.Errorf("factRecursive(-1) should be -1, got %d", r)
 	}
 
-	r = fact(0)
+	r = factRecursive(0)
 	if r != 1 {
-		t.Error("Fact(0) should be 1 ")
+		t.Errorf("factRecursive(0) should be 1, got %d", r)
 	}
 
-	r = fact(1)
-
+	r = factRecursive(1)
 	if r != 1 {
-		t.Error("Fact(1) should be 1 ")
+		t.Errorf("factRecursive(1) should be 1, got %d", r)
 	}
 
-	r = fact(5)
-
+	r = factRecursive(5)
 	if r != 120 {
-		t.Error("Fact(0) should be 120 ")
+		t.Errorf("factRecursive(5) should be 120, got %d", r)
+	}
+}
+
+func TestFactIter(t *testing.T) {
+	r := factIter(-1)
+	if r != -1 {
+		t.Errorf("factIter(-1) should be -1, got %d", r)
+	}
+
+	r = factIter(0)
+	if r != 1 {
+		t.Errorf("factIter(0) should be 1, got %d", r)
+	}
+
+	r = factIter(1)
+	if r != 1 {
+		t.Errorf("factIter(1) should be 1, got %d", r)
+	}
+
+	r = factIter(5)
+	if r != 120 {
+		t.Errorf("factIter(5) should be 120, got %d", r)
+	}
+}
+
+func BenchmarkFactRecusive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		factRecursive(500)
+	}
+}
+
+func BenchmarkFactIterative(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		factIter(500)
+	}
+}
+
+func BenchmarkFact(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fact(500)
 	}
 }
